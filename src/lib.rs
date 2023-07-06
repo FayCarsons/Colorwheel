cde809ecbb9ac91e03f22138c599fd01e0f059bc
Fay Carsons <faycarsons23@gmail.com>
2023-07-06 17:53:33 -0400

    implemented concurrency, runs in <2s. Prompts for cli args

--- src/lib.rs
+++ src/lib.rs
@@ -1,130 +1,183 @@
 use image::*;
 use rand::{self, Rng};
+use rayon::prelude::*;
+use std::boxed::Box;
+//use std::sync::Arc;
+use std::sync::atomic::{AtomicPtr, Ordering};
+
+#[derive(Clone, Copy, Debug)]
+pub struct Centroid {
+    r: f64,
+    g: f64,
+    b: f64,
+    num_pixels: u64
+}
+
+pub fn u8_to_f64(x: u8) -> f64 {
+    x as f64 / u8::MAX as f64
+}
+
+impl Centroid {
+    pub fn new(pixel: (f64, f64, f64)) -> Centroid {
+        let (r, g, b) = pixel;
+        Centroid {
+            r,
+            g,
+            b,
+            num_pixels: 1,
+        }
+    }
+
+    pub fn to_rgb(&self) -> (f64, f64, f64) {
+        (self.r, self.g, self.b)
+    }
+
+    pub fn average(&self) -> Centroid {
+        Centroid {
+            r: self.r / (self.num_pixels as f64),
+            g: self.g / (self.num_pixels as f64),
+            b: self.b / (self.num_pixels as f64),
+            num_pixels: 1,
+        }
+    }
+
+    pub fn to_rgb_u8(&self) -> (u8, u8, u8) {
+        (
+            (self.r as f64 * u8::MAX as f64) as u8,
+            (self.g as f64 * u8::MAX as f64) as u8,
+            (self.b as f64 * u8::MAX as f64) as u8,
+        )
+    }
+
+    #[inline(always)]
+    pub fn to_atom(self) -> AtomicPtr<Centroid> {
+        let ptr: *mut Centroid = Box::<Centroid>::into_raw(Box::new(self.clone()));
+        AtomicPtr::new(ptr)
+    }
+}
 
 pub fn get_raw_image(img: &DynamicImage) -> Vec<Vec<[u8; 4]>> {
-   let dim = img.dimensions();
-   (0..dim.1).map(|y|
-      (0..dim.0).map(|x|
-         img.get_pixel(x, y).0
-      ).collect()
-   ).collect()
+    let (x, y) = img.dimensions();
+    (0..y)
+        .into_par_iter()
+        .map(|y| {
+            (0..x)
+                .into_par_iter()
+                .map(|x| img.get_pixel(x, y).0)
+                .collect()
+        })
+        .collect()
 }
 
-pub fn init_centroids(data: &Vec<Vec<[u8; 4]>>, means: u16) 
-                     -> Vec<(f64,f64,f64)> {
-   let mut centroids = Vec::with_capacity(means as usize);   
-   let mut thread_rng = rand::thread_rng();
+pub fn init_centroids(data: &Vec<Vec<[u8; 4]>>, means: &u16) -> Vec<Centroid> {
+    let mut centroids: Vec<Centroid> = Vec::with_capacity(*means as usize);
+    let mut thread_rng = rand::thread_rng();
 
-   for _ in 0..means {
-      let rnd_x = thread_rng.gen_range(0..data[0].len());
-      let rnd_y = thread_rng.gen_range(0..data.len()); 
+    for _ in 0..*means {
+        let rnd_x = thread_rng.gen_range(0..data[0].len());
+        let rnd_y = thread_rng.gen_range(0..data.len());
 
-      let pixel = (data[rnd_y][rnd_x][0] as f64,
-                                    data[rnd_y][rnd_x][1] as f64,
-                                    data[rnd_y][rnd_x][2] as f64); 
+        let pixel = (
+            data[rnd_y][rnd_x][0] as f64 / 255.0,
+            data[rnd_y][rnd_x][1] as f64 / 255.0,
+            data[rnd_y][rnd_x][2] as f64 / 255.0,
+        );
 
-      centroids.push(pixel);
-   }                                                                                
-centroids                                                   
+        centroids.push(Centroid::new(pixel));
+    }
+    centroids
 }
 
+pub fn iterate(
+    img: &Vec<Vec<[u8; 4]>>,
+    centroids: &Vec<Centroid>,
+    x: usize,
+    y: usize,
+) -> Vec<Centroid> {
+    let new_centroids: Vec<AtomicPtr<Centroid>> = centroids
+        .iter()
+        .map(|c| c.to_atom())
+        .collect::<Vec<AtomicPtr<Centroid>>>();
 
+    (0..y).into_par_iter().for_each(|y| {
+        (0..x).for_each(|x| {
+            let new_pixel = (
+                img[y][x][0] as f64 / 255.0,
+                img[y][x][1] as f64 / 255.0,
+                img[y][x][2] as f64 / 255.0,
+            );
 
-
-
-pub fn iterate(data: &Vec<Vec<[u8; 4]>>, 
-               centroids: &Vec<(f64, f64, f64)>, 
-               p: &f64) 
-               -> Vec<(f64, f64, f64)> {
-   let mut container = vec![(0.0,0.0,0.0); centroids.len()];
-   let mut num_pixels = vec![0u32; centroids.len()];
-   
-   for y in 0..data.len() { 
-      for  x in 0..data[0].len() {
-         let pixel = (data[y][x][0] as f64, 
-                                       data[y][x][1] as f64,
-                                       data[y][x][2] as f64);
-         let mut min_dist = u64::MAX as f64;
-         let mut centroid_id = usize::MAX;
-      
-         for (c, val) in centroids.iter().enumerate() {
-            let dist = minkowski_distance(val, &pixel, *p);
-            if dist < min_dist {
-               min_dist = dist;
-               centroid_id = c;
+            let mut min_dist = u64::MAX as f64;
+            let mut centroid_id = centroids.len() + 1;
+            for (c, centroid) in centroids.iter().enumerate() {
+                let dist = minkowski_distance(&centroid.to_rgb(), &new_pixel, 2.0);
+                if dist < min_dist {
+                    min_dist = dist;
+                    centroid_id = c;
+                }
             }
-         }
-
-         container[centroid_id] = tup3_add(container[centroid_id], &pixel);
-         num_pixels[centroid_id] += 1;
-      }
-   }
-   
-   for (i, val) in container.iter_mut().enumerate() {
-      val.0 /= num_pixels[i] as f64;
-      val.1 /= num_pixels[i] as f64;
-      val.2 /= num_pixels[i] as f64;
-
-   }
-   container
-}
-
-
-
-
-
-
-pub fn create_img (mut target: image::RgbaImage, 
-                  data: &Vec<Vec<[u8; 4]>>, 
-                  centroids: &Vec<(f64, f64, f64)>, p: &f64) 
-                  -> RgbaImage {
-
-   let dim = (target.dimensions().0 as usize, 
-                              target.dimensions().1 as usize); 
-   
-   for x in 0..dim.0 {
-      for y in 0..dim.1 {
-         let r = data[y][x][0] as f64;
-         let g = data[y][x][1] as f64;
-         let b = data[y][x][2] as f64;
-         let data = (r, g, b);
-
-         let mut min_dist = u64::MAX as f64;
-         let mut centroid_id = 0; 
-         
-         for (c, val) in centroids.iter().enumerate() 
-         {
-            let dist = minkowski_distance(val, &data, *p);
-            if dist < min_dist {
-               min_dist = dist;
-               centroid_id = c;
+            let atom = new_centroids[centroid_id].load(Ordering::Relaxed);
+            unsafe {
+                new_centroids[centroid_id].store(
+                    Box::into_raw(Box::new(Centroid {
+                        r: (*atom).r + new_pixel.0,
+                        g: (*atom).g + new_pixel.1,
+                        b: (*atom).b + new_pixel.2,
+                        num_pixels: (*atom).num_pixels + 1,
+                    })),
+                    Ordering::Relaxed,
+                );
             }
-         }
-         let (out_r, out_g, out_b) = centroids[centroid_id];
-         let pixel: Rgba<u8> = Rgba([out_r as u8, out_g as u8, out_b as u8, 1]);
-         target.put_pixel(x as u32, y as u32, pixel);
-      }
-   }
-   target
+        })
+    });
+    let new_centroids = new_centroids
+        .iter()
+        .map(|c| unsafe { *c.load(Ordering::Relaxed) }.average())
+        .collect();
+    new_centroids
 }
 
-pub fn tup3_add(mut tuple_one: (f64, f64, f64), tuple_two: &(f64, f64, f64)) 
-               -> (f64, f64, f64) {
-   tuple_one.0 += tuple_two.0;
-   tuple_one.1 += tuple_two.1;
-   tuple_one.2 += tuple_two.2;
-   
-   tuple_one
+fn nearest_color(
+    x: usize,
+    y: usize,
+    data: &Vec<Vec<[u8; 4]>>,
+    centroids: &Vec<Centroid>,
+    p: f64,
+) -> Rgb<u8> {
+    let [r, g, b, _] = data[y][x];
+    let pixel = (u8_to_f64(r), u8_to_f64(g), u8_to_f64(b));
+    let mut min_dist = u64::MAX as f64;
+    let mut centroid_id = 0;
+
+    for (c, val) in centroids.iter().enumerate() {
+        let dist = minkowski_distance(&val.to_rgb(), &pixel, p);
+        if dist < min_dist {
+            min_dist = dist;
+            centroid_id = c;
+        }
+    }
+
+    let (out_r, out_g, out_b) = centroids[centroid_id].to_rgb_u8();
+    image::Rgb([out_r, out_g, out_b])
 }
 
-// minkowski distance function 
+pub fn create_img(
+    size: (u32, u32),
+    data: Vec<Vec<[u8; 4]>>,
+    centroids: Vec<Centroid>,
+    p: f64,
+) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let (x_size, y_size) = size;
+    ImageBuffer::from_fn(x_size, y_size, |x,y| 
+        nearest_color(x as usize, y as usize, &data, &centroids,p))  
+} 
+
+// 3D minkowski distance function
 // p=1 manhattan, p=2 euclidean, p=25+ chebyshev
-pub fn minkowski_distance (to: &(f64, f64, f64), 
-                        from: &(f64, f64, f64), 
-                        p: f64 ) -> f64 {
-   let dx = (to.0 - from.0).powf(p);
-   let dy = (to.1 - from.1).powf(p);
-   let dz = (to.2 - from.2).powf(p);
-   
-   (dx.abs() + dy.abs() + dz.abs()).powf(1.0 / p)
+pub fn minkowski_distance(to: &(f64, f64, f64), from: &(f64, f64, f64), p: f64) -> f64 {
+    let dx = (to.0 - from.0).powf(p);
+    let dy = (to.1 - from.1).powf(p);
+    let dz = (to.2 - from.2).powf(p);
+
+    (dx.abs() + dy.abs() + dz.abs()).powf(1.0 / p)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,62 @@
 mod lib;
-use lib::{init_centroids, iterate, create_img, get_raw_image};
+use lib::{create_img, get_raw_image, init_centroids, iterate, Centroid};
+
+use crate::imageops::{dither, ColorMap};
 use image::*;
-use std::str::{FromStr};
+use std::io::{stdin};
 use std::env;
+use std::str::FromStr;
 use std::time::Instant;
 
-// Command line args as follows - path to input image, path of output image, 
-// k value, minkowski distance p value, # of iterations
+fn prompt(line: &str) -> String {
+    let mut input = String::new();
+    println!("{}", line);
+    stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
+
+fn get_args() -> (String, String, u16, u16, f64) {    
+    let input_path = prompt("Enter input path: ");
+    let output_path = prompt("Enter output path: ");
+    let k = u16::from_str(&prompt("Enter K value: ")).unwrap();
+    let iterations = u16::from_str(&prompt("Enter # of iterations: ")).unwrap();
+    let p = f64::from_str(&prompt("Enter distance metric: ")).unwrap();
+    
+    (input_path, output_path, k, iterations, p)
+}
 
 fn main() {
-   // get params from command line args
-   let in_path = env::args().nth(1).unwrap();
-   let out_path = env::args().nth(2).unwrap();
+    // get command line args
+    let (in_path, out_path, k, iterations, distance) = get_args();    
 
-   let means = u16::from_str(&env::args().nth(3).unwrap()).unwrap();
-   let distance = f64::from_str(&env::args().nth(4).unwrap()).unwrap();
-   let iterations = u16::from_str(&env::args().nth(5).unwrap()).unwrap();
+    // get start time
+    let start = Instant::now();
 
-   // get start time
-   let start = Instant::now();
+    // load image, get dimensions and raw RGBA data as 2d vec of 4 element arrays
+    let img: DynamicImage = image::open(in_path).unwrap();
+    let (x_size, y_size) = img.dimensions();
+    let raw_data: Vec<Vec<[u8; 4]>> = get_raw_image(&img);
+    let raw_time = start.elapsed();
+    println!("Acquired raw image data in {raw_time:?}");
 
-   // load image, get dimensions and raw RGBA data as 2d vec of 4 element arrays
-   let img: DynamicImage = image::open(in_path).unwrap();
-   let dim: (u32, u32) = img.dimensions();
-   let raw_data = get_raw_image(&img);
-   
-   // place centroids, random points on image
-   let mut centroids: Vec<(f64, f64, f64)> = init_centroids(&raw_data, means);
+    // place centroids, random pixels in image
+    let mut centroids: Vec<Centroid> = init_centroids(&raw_data, &k);
 
-   // iterate centroids/do clustering
-   for _ in 0..=iterations {
-      centroids = iterate(&raw_data, &centroids, &distance);
-   }
-   
-   // create final image
-   let mut final_img: RgbaImage = RgbaImage::new(dim.0, dim.1);
-   final_img = create_img(final_img, &raw_data, &centroids, &distance);
-   
-   // save image
-   final_img.save(out_path).unwrap();
+    // iterate centroids/do clustering
+    for _ in 0..iterations {
+        centroids = iterate(&raw_data, &centroids, x_size as usize, y_size as usize);
+    }
+    let iteration_time = start.elapsed();
+    println!("Iterations completed in {iteration_time:?}");
 
-   // get duration 
-   let duration = start.elapsed();
-   println!("Executed In {duration:?} Seconds");
-} 
+    // create final image
+    let final_img = create_img(img.dimensions(), raw_data, centroids, distance);
+    // save image
+    final_img.save(out_path).unwrap();
+    let img_time = start.elapsed() - iteration_time;
+    println!("Image assembled and saved in {img_time:?}");
+
+    // get duration
+    let duration = start.elapsed();
+    println!("Executed In {duration:?}");
+}
